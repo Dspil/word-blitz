ac7653a8155266922fc8a04db0124f2eaef208c0
Dspil <dennisspiliopoylos@gmail.com>
2020-01-04 13:33:02 +0200

    started word_finder.py

---
+++ word_finder.py
@@ -0,0 +1,11 @@
+from tree_struct import *
+
+#initialize and load lexicon
+lex = lexicon()
+lex.load("lexicon.wb")
+
+
+#board is given
+board = [['N', 'K', 'K', 'J', 'Q'], ['O', 'D', 'Q', 'G', 'E'], ['X', 'I', 'E', 'L', 'F'], ['U', 'T', 'S', 'Z', 'U'], ['W', 'U', 'L', 'J', 'Z']]
+
+lex.display()
