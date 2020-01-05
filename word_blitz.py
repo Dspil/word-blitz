f1be05e873cdc7b828ca665dab5359fa645046a8
Dspil <dennisspiliopoylos@gmail.com>
2020-01-05 19:00:03 +0200

    added small delay at startup

--- word_blitz.py
+++ word_blitz.py
@@ -1,12 +1,13 @@
 from get_board import get_board
 from make_moves import move
 from word_finder import run_board
-from time import time
+from time import time,sleep
 
 def string_from_board(board, pos):
     return "".join([board[i][j] for i,j in pos])
 
 def main():
+    sleep(0.5)
     start = time()
     found = set()
     board, posboard = get_board()
@@ -26,3 +27,5 @@ def main():
         found.add(s)
         move(word, posboard)
     
+if __name__ == '__main__':
+    main()
